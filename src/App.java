2ba95557aa96ef4774b25c2d4f5562e75e7b88a3
ZUR1C4T0 <ivanulloque19@gmail.com>
2023-03-30 15:51:57 -0500

    proyecto iniciado

---
+++ src/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+
+    /**
+     * @param args los argumentos de la línea de comando
+     */
+    public static void main(String[] args) {
+        // TODO lógica de aplicación de código aquí
+    }
+
+}
