8993f78e7de94c7e35abb8628d3013cd6bd4a86d
Ivan Ulloque <95107566+ZUR1C4T0@users.noreply.github.com>
2023-04-03 02:16:24 +0000

    primera version del menu principal

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class App {
 
@@ -5,7 +6,26 @@ public class App {
      * @param args los argumentos de la línea de comando
      */
     public static void main(String[] args) {
-        // TODO lógica de aplicación de código aquí
+        Scanner scanner = new Scanner(System.in);
+
+        int mainAction;
+
+        System.out.println("--------- MENÚ PRINCIPAL ---------");
+        System.out.println("1. Agregar nuevo empreado");
+        System.out.println("2. Imprimir nómina");
+
+        while (true) {
+            System.out.print("Digite una opción: ");
+            mainAction = scanner.nextInt();
+            if (mainAction == 1 || mainAction == 2) {
+                break;
+            }
+            System.out.println("----- OPCIÓN INVALIDA -----");
+        }
+
+        System.out.println("opción: " + mainAction);
+
+        scanner.close();
     }
 
 }
